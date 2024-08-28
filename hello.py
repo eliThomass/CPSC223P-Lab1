c6be3ec3373d587b69d13f0325fc242500c6a692
eliThomass <ecthomas05@gmail.com>
2024-08-28 13:49:49 -0700

    added hello.py, main.py files, added credentials

---
+++ hello.py
@@ -0,0 +1,10 @@
+# Name: Eli Thomas
+# Date: 8/28/2024
+# File Purpose: Multiple hello functions
+
+def helloworld():
+	return "Hello World!"
+	
+def helloname(name):
+	return "Hello " + name + "!"
+
---
+++ main.py
@@ -0,0 +1,3 @@
+# Name: Eli Thomas
+# Date: 8/28/2024
+# File Purpose: Lab01 main program driver program
