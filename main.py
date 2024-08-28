f709504c1fec4c670826f2469fd2cbc9953a3991
eliThomass <ecthomas05@gmail.com>
2024-08-28 13:51:29 -0700

    finished main.py

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 # Name: Eli Thomas
 # Date: 8/28/2024
 # File Purpose: Lab01 main program driver program
+
+from hello import *
+
+print("*** TUFFY TITAN HELLO PROGRAM ***")
+
+print(helloworld())
+
+print(helloname("Eli")
